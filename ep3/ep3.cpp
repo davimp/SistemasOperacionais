23a7ae7978c6731158cec552e1a341e465cdebe4
davimp <davimp2009@gmail.com>
2020-11-24 17:57:18 -0300

    cp e mkdir iniciados

--- ep3/ep3.cpp
+++ ep3/ep3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #define MAXN 25000 /* quantidade de blocos */
 #define BLOCO 4000 /* cada bloco pode ter até quatro mil caracteres */
 
@@ -9,6 +9,9 @@ using namespace std;
 
 struct Arquivo{
     string nome;
+    string diretorio;
+    string conteudo;
+    int inicio;
     int tamanho;
     int criado;
     int modificado;
@@ -17,24 +20,48 @@ struct Arquivo{
 
 struct Diretorio{
     string nome;
-    int tamanho;
+    vector<int> arquivos;
     int criado;
     int modificado;
     int acessado;
-} ;
+};
 
 int bitmap[MAXN];
 int FAT[MAXN];
-vector<Diretorio> grafo[MAXN]; //mkdir /a/b/c
-map<string, Diretorio> dir[MAXN];
+int num_diretorios, num_arquivos;
+Diretorio diretorios[MAXN];
+Arquivo arquivos[MAXN];
+vector<int> grafo[MAXN];
+unordered_map<string, int> d, a; /* liga o diretório ao seu id */
+
+int proximo_livre(){
+    int x;
+    for(x = 39; x < MAXN; x++){
+        if(!bitmap[x]){
+            break;
+        }
+    }
+    return x;
+}
+
+void imprime_arquivos(int id){
+    for(int it : diretorios[id].arquivos){
+        cout << arquivos[it].nome << endl;
+    }
+    for(int it : grafo[id]){
+        imprime_arquivos(it);
+    }    
+}
 
 int main(int argc, char *argv[]){
-    int i,j;
-    string nome_arq, s;
+    int i,j,k;
+    string nome_arq, s, conteudo, aux;
     string linha_lida, comando, argumentos[10];
-    fstream arq;
+    fstream arq, original;
     streampos pos;
-
+    num_diretorios = num_arquivos = 0;
+    d["/"] = 0;
+    num_diretorios++;
     /* espaço ocupado pelo fat e pelo bitmap no início do arquivo */
     for(i = 0; i < 39; i++){
         bitmap[i] = 1;
@@ -59,7 +86,7 @@ int main(int argc, char *argv[]){
             cout << s << endl;
 
             if(arq.eof())
-            {   /*125/4 25/4*/
+            {   /* 125/4 25/4*/
                 for(i = 0; i < (32 + 7)*BLOCO; i++)
                     arq << '0';
             }
@@ -68,23 +95,55 @@ int main(int argc, char *argv[]){
         else if(comando == "cp"){
             /*origem destino*/
             cin >> argumentos[0] >> argumentos[1];
+            i = proximo_livre();
+            
+            /*crio o arquivo no bloco i*/
+            if(i < MAXN)
+            {
+                /* pega o arquivo original */
+                original.open(argumentos[0], fstream::in | fstream::out);
+
+                aux = argumentos[1].substr(argumentos[1].find_last_of("/")+1, argumentos[1].length());
+                a[argumentos[1]] = num_arquivos;
+                argumentos[1] = argumentos[1].substr(0, argumentos[1].find_last_of("/")+1);
+                j = d[argumentos[1]];
+                (diretorios[j].arquivos).push_back(num_arquivos);
+
+                /* cria o arquivo */
+                arquivos[num_arquivos].nome = aux;
+                original >> arquivos[num_arquivos].conteudo;
+                arquivos[num_arquivos].tamanho = (arquivos[num_arquivos].conteudo).length();
+                arquivos[num_arquivos].diretorio = argumentos[1];
+                /* arquivos[num_arquivos].criado */
+                /* arquivos[num_arquivos].modificado */
+                /* arquivos[num_arquivos].acessado */
+                num_arquivos++;
+                original.close();
+            }
+            else
+                cout << "Lotado" << endl;
+
+            /* fecha original */
         }
         else if(comando == "mkdir"){
             /*diretorio*/
             cin >> argumentos[0];
-
-
-            for(i = 39; i < 25000; i++){
-                if(!bitmap[i]){
-                    break;
-                }
-            }
-
+            i = proximo_livre();
 
             /*crio o diretorio no bloco i*/
-            if(i < 25000)
+            if(i < MAXN)
             {
-                
+                d[argumentos[0]] = num_diretorios;
+                aux = argumentos[0].substr(0, argumentos[0].find_last_of("/")+1);
+                j = d[aux];
+                grafo[j].push_back(num_diretorios);
+
+                diretorios[num_diretorios].nome = argumentos[0];
+                /*diretorios[num_diretorios].criado*/
+                /*diretorios[num_diretorios].modificado*/
+                /*diretorios[num_diretorios].acessado*/
+                d[argumentos[0]] = num_diretorios;
+                num_diretorios++;
             }
             else
                 cout << "Lotado" << endl;
@@ -95,16 +154,20 @@ int main(int argc, char *argv[]){
             cin >> argumentos[0];
         }
         else if(comando == "cat"){
-            /*arquivo*/
+            /* arquivo */
             cin >> argumentos[0];
+            j = a[argumentos[0]];
+            cout << arquivos[j].conteudo << endl;
         }
         else if(comando == "touch"){
-            /*arquivo*/
+            /* arquivo */
             cin >> argumentos[0];
         }
         else if(comando == "ls"){
-            /*arquivo*/
+            /* diretorio */
             cin >> argumentos[0];
+            j = d[argumentos[0]];
+            imprime_arquivos(j);
         }
         else if(comando == "find"){
             /*diretorio arquivo*/
@@ -112,7 +175,7 @@ int main(int argc, char *argv[]){
         }
         else if(comando == "df"){
         }
-        else if(comando == "unmount"){
+        else if(comando == "umount"){
             arq.close();
             /*writer*/
         }
--- ep3/teste.cpp
+++ ep3/teste.cpp
@@ -11,61 +11,16 @@ int FAT[MAXN];
 int main(int argc, char *argv[]){
 
     int i,j;
-    string nome_arq;
+    string nome_arq, aux, conteudo;
     string linha_lida, comando, argumentos[10];
-    ofstream arq;
+    fstream arq;
 
-    //Shell
-    while(1)
-    {
-        cout << "[ep3]: ";
-        /*getline(cin, linha_lida);*/
-        /*comando = linha_lida.substr(0,linha_lida.find(" "));*/
-        cin >> comando;
+    cin >> aux;
+    aux = aux.substr(aux.find_last_of("/")+1, aux.length());
+    cout << aux << endl;
 
-        if(comando == "mount"){
-            /* arquivo */
-            cin >> argumentos[0];
-            cout << "monte " << argumentos[0] << endl;
-        }
-        else if(comando == "cp"){
-            /*origem destino*/
-            cin >> argumentos[0] >> argumentos[1];
-        }
-        else if(comando == "mkdir"){
-            /*diretorio*/
-            cin >> argumentos[0];
-        }
-        else if(comando == "rmdir"){
-            /*diretorio*/
-            cin >> argumentos[0];
-        }
-        else if(comando == "cat"){
-            /*arquivo*/
-            cin >> argumentos[0];
-        }
-        else if(comando == "touch"){
-            /*arquivo*/
-            cin >> argumentos[0];
-        }
-        else if(comando == "ls"){
-            /*arquivo*/
-            cin >> argumentos[0];
-        }
-        else if(comando == "find"){
-            /*diretorio arquivo*/
-            cin >> argumentos[0] >> argumentos[1];
-        }
-        else if(comando == "df"){
-        }
-        else if(comando == "unmount"){
-        }
-        else if(comando == "sai"){
-        }
-        else{
-            cout << "comando inválido!\n";
-        }
-    }
-
-    return 0;
+    cin >> aux;
+    arq.open(aux, fstream::out | fstream::in);
+    arq >> conteudo;
+    cout << conteudo << endl;
 }
